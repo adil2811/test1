ce4859a7e4dc0f0cdedc0a8af7798d0f746abb1d
adil2811 <128904178+adil2811@users.noreply.github.com>
2024-01-02 18:14:09 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,7 @@ export default function Home() {
   const what_to_expect = course.what_to_expect
   const key_topics = course.key_topics
 console.log('✌️what_to_expect --->', what_to_expect);
+
   const imageUrl =
     "https://imgs.search.brave.com/yjwu6F1mDOozAW88aPZ3y7toVpLhs1tzKHzf9I4kohM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvNTEx/OTU3MS5qcGc";
 
@@ -24,20 +25,30 @@ console.log('✌️what_to_expect --->', what_to_expect);
       {" "}
       {/* Adjust the margins based on your layout */}
       <div className="relative">
-        <img src={imageUrl} alt="Overlay" className="w-full h-[340px]" />
-        <div className="absolute top-1/2 left-0 transform -translate-y-1/2 text-white p-4">
+        <img src={imageUrl} alt="Overlay" className="w-full h-[440px]" />
+        <div className="absolute top-[70%] left-0 transform -translate-y-1/2 text-white p-4">
           <h1 className="mt-10 text-white">{instructor.name}</h1>
-          <span className="text-3xl text-white">
-            {course.title}
-          </span>
+          <br/>
+          
+          <span className="text-3xl text-white ">
+  {course.title.slice(0, 37)}<br/>
+  <span className="ml-2">
+    {course.title.slice(37,64)}<br/>
+    </span>
+    <span className="ml-10">
+
+    {course.title.slice(64)}
+    </span>
+</span>
+
         </div>
       </div>
       <div className="grid grid-cols-2 gap-4">
         <div className="bg-gray-100 p-4">
           <ul className="flex ml-20">
-            <li className="mr-10 ">About</li>
-            <li className="mr-10">Instructor</li>
-            <li>Reviews</li>
+            <li className="mr-10 hover:underline cursor-pointer">About</li>
+            <li className="mr-10 hover:underline cursor-pointer">Instructor</li>
+            <li className="hover:underline cursor-pointer">Reviews</li>
           </ul>
           <div className="mt-10">
             <h1 className="mt-10 text-2xl ml-10">
@@ -47,14 +58,18 @@ console.log('✌️what_to_expect --->', what_to_expect);
             {removeHtmlTags(about_instructor.html_content.slice(3,-5))}
             </p>
             <h2 className="mt-5 text-xl ml-10">
-              <b>what to expect from the course</b>
+              <b>What to expect from the course</b>
             </h2>
-              <div>
-              {removeHtmlTags(what_to_expect.html_content)}
+              <div className="ml-6">
+              {removeHtmlTags(what_to_expect.html_content.slice(0,42))}<br/>
+              {removeHtmlTags(what_to_expect.html_content.slice(42 ,71))}<br/>
+              {removeHtmlTags(what_to_expect.html_content.slice(71))}<br/>
+
+
               </div>
             <br />
             <span className="ml-10 text-xl mt-[10px]">
-              <b>key topics covered</b>
+              <b>Key topics covered</b>
             </span>
             <p className="text-justify px-6 mt-4">
             {removeHtmlTags(key_topics.html_content)}
@@ -77,18 +92,22 @@ console.log('✌️what_to_expect --->', what_to_expect);
             <li className="mt-2">live_qa_sessions:{course.inclusions.live_qa_sessions ? 'yes':'no'}</li>
             <li className="mt-2">whatsapp_community:{course.inclusions.whatsapp_community ? 'yes':'no'}</li>
           </ul>
-          <button class="flex bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-10 ml-20 mt-4 rounded-full items-center ">
+          <button className="flex bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-10 ml-20 mt-4 rounded-full items-center ">
   Register Now
 </button>
         </div>
       </div>
       <div>
-        <h3 className="mt-10 text-2xl ml-10">About the Instructor</h3>
+        <h3 className="mt-10 text-2xl ml-10">
+          <b>
+
+          About the Instructor
+          </b>
+          </h3>
         <div className="grid grid-cols-2 gap-4 mt-4 ">
           {/* Content for the grid columns */}
           <div className="bg-gray-100 p-4">
             <div className="flex">
-              <div className="w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center "></div>
               <p className="ml-10">
                 {removeHtmlTags(about_instructor.html_content)}
 
@@ -116,17 +135,19 @@ console.log('✌️what_to_expect --->', what_to_expect);
 
   <div className="flex items-center mt-4 px-20">
   <div className="w-10 h-10 bg-blue-500 rounded-full flex  justify-center mr-3 ">
-      <span className="text-white font-bold">A</span> {/* Replace with the initial of the author */}
+      <span className="text-white font-bold mt-2">A</span> {/* Replace with the initial of the author */}
     </div>
     <span className="font-semibold">
       {testimonial?.reviewer_name}<br/>
-      <span>{testimonial?.reviewer_designation}</span>
+      <span className="text-[12px] mt-[-2px]">{testimonial?.reviewer_designation}</span>
     </span>
     
   </div>
 </div>
 
+<div className="h-10">
 
+</div>
     </div>
   );
 }
