fe5062c8af170d8366b8a0788656505bde02dec6
adil2811 <128904178+adil2811@users.noreply.github.com>
2024-01-02 17:31:19 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,132 @@
+
+import Image from "next/image";
+import data from './data.json';
+
+export default function Home() {
+  console.log(data)
+  const course = data[0].course
+  const instructor = data[0].instructor
+  const about_instructor = data[0].about_instructor
+  const testimonial = data[0].testimonial
+  const what_to_expect = course.what_to_expect
+  const key_topics = course.key_topics
+console.log('✌️what_to_expect --->', what_to_expect);
+  const imageUrl =
+    "https://imgs.search.brave.com/yjwu6F1mDOozAW88aPZ3y7toVpLhs1tzKHzf9I4kohM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvNTEx/OTU3MS5qcGc";
+
+
+    function removeHtmlTags(html) {
+      return html.replace(/<[^>]*>?/gm, '');
+    }
+    
+  return (
+    <div className="mx-4 sm:mx-10 md:mx-20 lg:mx-32">
+      {" "}
+      {/* Adjust the margins based on your layout */}
+      <div className="relative">
+        <img src={imageUrl} alt="Overlay" className="w-full h-[340px]" />
+        <div className="absolute top-1/2 left-0 transform -translate-y-1/2 text-white p-4">
+          <h1 className="mt-10 text-white">{instructor.name}</h1>
+          <span className="text-3xl text-white">
+            {course.title}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="bg-gray-100 p-4">
+          <ul className="flex ml-20">
+            <li className="mr-10 ">About</li>
+            <li className="mr-10">Instructor</li>
+            <li>Reviews</li>
+          </ul>
+          <div className="mt-10">
+            <h1 className="mt-10 text-2xl ml-10">
+              <b>About the Course</b>
+            </h1>
+            <p className="text-justify px-6 mt-4">
+            {removeHtmlTags(about_instructor.html_content.slice(3,-5))}
+            </p>
+            <h2 className="mt-5 text-xl ml-10">
+              <b>what to expect from the course</b>
+            </h2>
+              <div>
+              {removeHtmlTags(what_to_expect.html_content)}
+              </div>
+            <br />
+            <span className="ml-10 text-xl mt-[10px]">
+              <b>key topics covered</b>
+            </span>
+            <p className="text-justify px-6 mt-4">
+            {removeHtmlTags(key_topics.html_content)}
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-100 w-[420px] h-[250px]  p-4 ml-[90px] mt-[-80px] z-10">
+          <h1>
+            <b>course fee</b>
+          </h1>
+          <span className="text-3xl">
+            <b>{course.fee.amount} {course.fee.currency? '₹':null}</b>
+          </span>
+
+          <h1 className="mt-3">
+            <b>what's include</b>
+          </h1>
+          <ul>
+            <li className="mt-2">on_demand_videos:{course.inclusions.on_demand_videos}</li>
+            <li className="mt-2">live_qa_sessions:{course.inclusions.live_qa_sessions ? 'yes':'no'}</li>
+            <li className="mt-2">whatsapp_community:{course.inclusions.whatsapp_community ? 'yes':'no'}</li>
+          </ul>
+          <button class="flex bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-10 ml-20 mt-4 rounded-full items-center ">
+  Register Now
+</button>
+        </div>
+      </div>
+      <div>
+        <h3 className="mt-10 text-2xl ml-10">About the Instructor</h3>
+        <div className="grid grid-cols-2 gap-4 mt-4 ">
+          {/* Content for the grid columns */}
+          <div className="bg-gray-100 p-4">
+            <div className="flex">
+              <div className="w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center "></div>
+              <p className="ml-10">
+                {removeHtmlTags(about_instructor.html_content)}
+
+              </p>
+            </div>
+          </div>
+          <div className="bg-gray-100 p-4">
+            test test test            test test test
+            test test test
+            test test test
+            test test test
+            test test test
+            test test test
+
+
+          </div>
+        </div>
+      </div>
+      <div className="mt-10 text-justify px-10">
+  <span className="block  break-words" style={{ textJustify: 'inter-word', textIndent: '1em' }}>
+    <b>
+      {testimonial?.text}
+    </b>
+  </span>
+
+  <div className="flex items-center mt-4 px-20">
+  <div className="w-10 h-10 bg-blue-500 rounded-full flex  justify-center mr-3 ">
+      <span className="text-white font-bold">A</span> {/* Replace with the initial of the author */}
+    </div>
+    <span className="font-semibold">
+      {testimonial?.reviewer_name}<br/>
+      <span>{testimonial?.reviewer_designation}</span>
+    </span>
+    
+  </div>
+</div>
+
+
+    </div>
+  );
+}
